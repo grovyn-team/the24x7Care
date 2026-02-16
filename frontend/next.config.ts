3392a89d662bfcf61c685dea04cd309af7c1fee9
Gaurav Singh <thakurgaurav273@gmail.com>
2026-02-17 00:16:46 +0530

    fix: disable lightningcss for netlify build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    experimental: {
+    useLightningcss: false, // ⭐ disables native lightningcss binary
+  },
 };
 
 export default nextConfig;
