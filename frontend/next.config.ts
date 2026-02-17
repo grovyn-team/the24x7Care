0178a6e688761945e197944685ffa81d1d96b464
Gaurav Singh <thakurgaurav273@gmail.com>
2026-02-17 22:49:50 +0530

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    experimental: {
-    useLightningcss: false, // ⭐ disables native lightningcss binary
-  },
 };
 
 export default nextConfig;
